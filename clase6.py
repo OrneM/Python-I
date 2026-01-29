5dcf23f35664ac0909e00eb359ece3314a61d1ae
OrneM <ornemans88@gmail.com>
2026-01-29 20:30:28 -0300

    Clase 6

---
+++ clase6.py
@@ -0,0 +1,24 @@
+
+
+nombres = []
+
+while True:
+    opcion = int(input("Seleccione una opción (1, 2, o 3): "))
+    if opcion == 1:
+        nombre = input("Ingrese un nombre: ")
+        nombres.append(nombre)
+    elif opcion == 2:
+        print("Nombres ingresados:")
+        for nombre in nombres:
+            print(nombre)
+    elif opcion == 3:
+        print("Saliendo del programa.")
+        break
+    else:
+        print("Opción no válida. Por favor, intente de nuevo.") 
+
+
+    
+
+
+
